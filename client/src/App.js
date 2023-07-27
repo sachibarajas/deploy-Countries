2e30b46c1d74ff85a31e359b3856bdb99b097088
Santiago Barajas <barajas03@gmail.com>
2023-07-26 21:35:35 -0500

    connection deploy

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { Route } from "react-router-dom";
 import {Home,Landing,Form,Detail} from './Views';
 import './App.css';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'https://countriesapi-wwvm.onrender.com';
 
 function App() {
   return (
